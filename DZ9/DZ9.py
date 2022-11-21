f88272b7568c3d2e8ba8b12f6ef3aeb1d610a3e8
Nemmo1 <kirya.gib@gmail.com>
2022-11-21 21:31:31 +0300

    dz 9
    Добавил 1, обернул все в функции

--- DZ9/DZ9.py
+++ DZ9/DZ9.py
@@ -1,10 +1,21 @@
-n = int(input())
-a =[list(map(int, input().split())) for i in range(n)]
-for i in range(n):
-    for j in range(n):
-        a[j].append(a[i][j])
-for k in range(n):
-    for i in range(n):
-        a[i].pop(0)
-for i in range(len(a)):
-    print(*a[i])
+def f():
+    from random import random as r
+    n,k=map(int,input('n k > ').split())
+    k-=1; b=range(n)
+    a=[[r() for j in b] for i in b]; print(a)
+    for j in b: a[k][j]/=a[k][k]
+    print(a)
+f()
+
+#def f():
+#   n = int(input())
+#   a =[list(map(int, input().split())) for i in range(n)]
+#   for i in range(n):
+#        for j in range(n):
+#            a[j].append(a[i][j])
+#   for k in range(n):
+#        for i in range(n):
+#            a[i].pop(0)
+#   for i in range(len(a)):
+#        print(*a[i])
+#f()
