e74831d370b5590211230234a5f8dc3c745eb87c
Nemmo1 <kirya.gib@gmail.com>
2022-11-28 20:46:15 +0300

    10 change

--- dz10/dz10.py
+++ dz10/dz10.py
@@ -1,27 +1,3 @@
-line = f.readlines()
-l = [element.replace ("\n", "").split() for element in line]
-n = len(l)
-m = len(l[0])
-a = l
-for i in range(n):
-    for k in range(m):
-        a[i][k] = int(a[i][k])
-    print()
-
-p = n
-k = int(input(f'Выберите номер строки из {p} '))
-k -= 1
-b=range(n)
-for j in b:
-    a[k][j]/=a[k][k]
-
-with open('ZhiboedovKV_u224_vivod.txt', 'w') as f2:
-    for i in range(n):
-        for k in range(m):
-            f2.write(str(a[i][k]))
-            f2.write(' ')
-        f2.write('\n')
-
 with open('ZhiboedovKV_u224_vvod.txt', 'r+') as f:
     line = f.readlines()
     l = [element.replace ("\n", "").split() for element in line]
@@ -33,17 +9,39 @@ for i in range(n):
         a[i][k] = int(a[i][k])
     print()
 
-res=[]
-for j in range(m):
-    tmp=[]
-    for i in range(n):
-        tmp=tmp+[a[i][j]]
-    res=res+[tmp]
+def pr(mas):
+    p = n
+    k = int(input(f'Выберите номер строки из {p} '))
+    k -= 1
+    b=range(n)
+    for j in b:
+        mas[k][j]/=mas[k][k]
+    return mas
+
+with open('ZhiboedovKV_u224_vivod.txt', 'w') as f2:
+    f2.write(str(pr(a)))
+    f2.write('\n')
+
+with open('ZhiboedovKV_u224_vvod.txt', 'r+') as f:
+    line = f.readlines()
+    l = [element.replace ("\n", "").split() for element in line]
+n = len(l)
+m = len(l[0])
+a = l
+for i in range(n):
+    for k in range(m):
+        a[i][k] = int(a[i][k])
+    print()
+def transponse(matr):
+    res=[]
+    for j in range(m):
+        tmp=[]
+        for i in range(n):
+            tmp=tmp+[matr[i][j]]
+        res=res+[tmp]
+    return res
 
 with open('ZhiboedovKV_u224_vivod.txt', 'a+') as f2:
     f2.write('\n')
-    for i in range(n):
-        for k in range(m):
-            f2.write(str(res[i][k]))
-            f2.write(' ')
-        f2.write('\n')
+    f2.write(str(transponse(a)))
+    f2.write('\n')
